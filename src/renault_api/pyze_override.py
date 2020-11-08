a7c1083b8567135844023f590673af91d6ea61ec
epenet <6771947+epenet@users.noreply.github.com>
2020-11-08 22:57:40 +0100

    Add PyZE overrides

---
+++ src/renault_api/pyze_override.py
@@ -0,0 +1,44 @@
+"""Override classes for PyZE."""
+from typing import Optional
+
+from pyze.api.credentials import PERMANENT_KEYS  # type: ignore
+from pyze.api.kamereon import Kamereon  # type: ignore
+
+from .const import CONF_GIGYA_URL
+from .const import CONF_KAMEREON_APIKEY
+from .const import CONF_KAMEREON_URL
+from .const import CONF_LOCALE
+
+# For some reason, PyZE only puts the API keys as permanent keys!
+# We want all these to stay permanent.
+PERMANENT_KEYS.extend([CONF_GIGYA_URL, CONF_KAMEREON_URL, CONF_LOCALE])
+
+
+# For some reason, PyZE writes the account id into the credential store!
+# This makes it very difficult to have parallel Kamereon objects.
+class KamereonOverride(Kamereon):  # type: ignore
+    """Override Kamereon object."""
+
+    def __init__(
+        self,
+        credentials,
+        gigya,
+        country,
+    ) -> None:
+        """Initialise Kamereon override."""
+        super().__init__(
+            api_key=credentials[CONF_KAMEREON_APIKEY],
+            credentials=credentials,
+            gigya=gigya,
+            country=country,
+            root_url=credentials[CONF_KAMEREON_URL],
+        )
+        self._account_id: Optional[str] = None
+
+    def set_account_id(self, account_id: str) -> None:
+        """Set account id."""
+        self._account_id = account_id
+
+    def get_account_id(self) -> str:
+        """Get account id."""
+        return self._account_id
