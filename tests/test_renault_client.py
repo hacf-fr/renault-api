e1b0a72be47e49a0e48fde6b77978a7bf06e772c
epenet <6771947+epenet@users.noreply.github.com>
2020-11-13 21:08:03 +0100

    Update method name

--- tests/test_renault_client.py
+++ tests/test_renault_client.py
@@ -2,7 +2,7 @@
 from renault_api.renault_client import RenaultClient
 
 
-def test_renault_client() -> RenaultClient:
+def test_initialise_renault_client() -> RenaultClient:
     """Ensure RenaultClient can be initialised."""
     client = RenaultClient()
     return client
