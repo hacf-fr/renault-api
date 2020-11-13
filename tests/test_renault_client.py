5e7fe9efceb223b8fa25246ce8dcfe8179570aff
epenet <6771947+epenet@users.noreply.github.com>
2020-11-13 21:13:08 +0100

    Move RenaultClient to new module (#27)
    * Move RenaultClient to new module
    
    * Update header
    
    * Update method name

---
+++ tests/test_renault_client.py
@@ -0,0 +1,8 @@
+"""Tests for RenaultClient."""
+from renault_api.renault_client import RenaultClient
+
+
+def test_initialise_renault_client() -> RenaultClient:
+    """Ensure RenaultClient can be initialised."""
+    client = RenaultClient()
+    return client
