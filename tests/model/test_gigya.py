15da5e546659effeb8a430186bc80fb7ebbea913
epenet <6771947+epenet@users.noreply.github.com>
2020-11-17 15:57:37 +0100

    Update tests

--- tests/model/test_gigya.py
+++ tests/model/test_gigya.py
@@ -1,5 +1,4 @@
 """Tests for RenaultClient."""
-import ast
 from typing import Any
 from typing import Type
 from marshmallow.schema import Schema
@@ -18,14 +17,14 @@ from renault_api.model.gigya import GigyaLoginResponseSchema
 def get_response_content(path: str, schema: Type[Schema]) -> Any:
     """Read fixture text file as string."""
     with open(f"tests/fixtures/gigya/{path}", "r") as file:
-        dict_value = ast.literal_eval(file.read())
-    return schema.load(dict_value)
+        content = file.read()
+    return schema.loads(content)
 
 
 def test_login_response() -> None:
     """Test login response."""
     response: GigyaLoginResponse = get_response_content(
-        "login.txt", GigyaLoginResponseSchema
+        "login.json", GigyaLoginResponseSchema
     )
     assert response.sessionInfo.cookieValue == "sample-cookie-value"
 
@@ -33,7 +32,7 @@ def test_login_response() -> None:
 def test_login_failed_response() -> None:
     """Test login response."""
     response: GigyaLoginResponse = get_response_content(
-        "login_failed.txt", GigyaLoginResponseSchema
+        "login_failed.json", GigyaLoginResponseSchema
     )
     with pytest.raises(GigyaResponseException) as excinfo:
         response.raise_for_error_code()
@@ -44,34 +43,16 @@ def test_login_failed_response() -> None:
 def test_get_account_info_response() -> None:
     """Test login response."""
     response: GigyaGetAccountInfoResponse = get_response_content(
-        "account_info.txt", GigyaGetAccountInfoResponseSchema
+        "account_info.json", GigyaGetAccountInfoResponseSchema
     )
     response.raise_for_error_code()
     assert response.data.personId == "person-id-1"
 
 
-def test_get_account_info_corrupted_response() -> None:
-    """Test login response."""
-    response: GigyaGetAccountInfoResponse = get_response_content(
-        "account_info_corrupted.txt", GigyaGetAccountInfoResponseSchema
-    )
-    response.raise_for_error_code()
-    assert response.data is None
-
-
 def test_get_jwt_response() -> None:
     """Test login response."""
     response: GigyaGetJWTResponse = get_response_content(
-        "get_jwt.txt", GigyaGetJWTResponseSchema
+        "get_jwt.json", GigyaGetJWTResponseSchema
     )
     response.raise_for_error_code()
     assert response.id_token == "sample-jwt-token"
-
-
-def test_get_jwt_corrupted_response() -> None:
-    """Test login response."""
-    response: GigyaGetJWTResponse = get_response_content(
-        "get_jwt_corrupted.txt", GigyaGetJWTResponseSchema
-    )
-    response.raise_for_error_code()
-    assert response.id_token is None
