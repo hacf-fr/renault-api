089b8a2b19995f1c250263155d88873a9ee2555c
epenet <6771947+epenet@users.noreply.github.com>
2020-11-20 10:10:27 +0100

    Add CredentialStore

---
+++ src/renault_api/credential_store.py
@@ -0,0 +1,98 @@
+"""Kamereon client for interaction with Renault servers."""
+import json
+import os
+from typing import Dict
+from typing import Optional
+
+from renault_api.model.credential import Credential, JWTCredential
+
+
+PERMANENT_KEYS = ["locale"]
+
+
+class CredentialStore:
+    """Credential store."""
+
+    def __init__(self):
+        """Initialise the credential store."""
+        self._store: Dict[str, Credential] = {}
+
+    def __getitem__(self, name: str) -> Credential:
+        """Get a credential the credential store."""
+        if name in list(self._store.keys()):
+            cred = self._store[name]
+            if not cred.has_expired():
+                return cred
+        raise KeyError(name)
+
+    def get(self, name: str) -> Optional[Credential]:
+        """Get a credential the credential store."""
+        if name in list(self._store.keys()):
+            cred = self._store[name]
+            if not cred.has_expired():
+                return cred
+        return None
+
+    def __setitem__(self, name: str, value: Credential):
+        """Add a credential to the credential store."""
+        if not isinstance(name, str):  # pragma: no cover
+            raise TypeError("`name` must be a string")
+        if not isinstance(value, Credential):  # pragma: no cover
+            raise TypeError("`value` must be a Credential")
+
+        self._store[name] = value
+        self._write()
+
+    def __contains__(self, name) -> bool:
+        """Check if a credential is in the credential store."""
+        if name in self._store:
+            cred = self._store[name]
+            if not cred.has_expired():
+                return True
+        return False
+
+    def _write(self):
+        """Writes the content to fixed storage."""
+        pass
+
+    def clear(self):
+        """Remove all non-permanent keys."""
+        for key in list(self._store.keys()):
+            if key not in PERMANENT_KEYS:
+                del self._store[key]
+        self._write()
+
+
+class CredentialEncoder(json.JSONEncoder):
+    def default(self, obj: Credential):
+        return obj.value
+
+
+class FileCredentialStore(CredentialStore):
+    """Credential store with items stored in a file."""
+
+    def __init__(self, store_location):
+        """Initialise the credential store."""
+        super().__init__()
+        self._store_location = store_location
+        self._read()
+
+    def _read(self) -> None:
+        """Read data from store location."""
+        if not os.path.exists(self._store_location):
+            return
+        with open(self._store_location) as json_file:
+            data = json.load(json_file)
+            for key, value in data.items():
+                if key == "gigya_jwt":
+                    self[key] = JWTCredential(value)
+                else:
+                    self[key] = Credential(value)
+
+    def _write(self) -> None:
+        """Write data to store location."""
+        dirname = os.path.dirname(self._store_location)
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
+        with open(self._store_location, "w") as json_file:
+            json.dump(self._store, json_file, cls=CredentialEncoder)
---
+++ tests/test_credential_store.py
@@ -0,0 +1,147 @@
+"""Test cases for the Gigya client."""
+import tempfile
+import time
+from shutil import copyfile
+from unittest import mock
+
+import pytest
+from tests import get_jwt
+
+from renault_api.credential_store import CredentialStore
+from renault_api.credential_store import FileCredentialStore
+from renault_api.model.credential import Credential
+from renault_api.model.credential import JWTCredential
+
+
+def test_invalid_credential() -> None:
+    """Test get/set with simple credential."""
+    credential_store = CredentialStore()
+
+    test_key = "test"
+    with pytest.raises(TypeError):
+        credential_store[test_key] = test_key
+
+    test_value = Credential("test_value")
+    with pytest.raises(TypeError):
+        credential_store[test_value] = test_value
+
+
+def test_simple_credential() -> None:
+    """Test get/set with simple credential."""
+    credential_store = CredentialStore()
+    test_key = "test"
+
+    # Try to get value from empty store
+    assert test_key not in credential_store
+    assert not credential_store.get(test_key)
+    with pytest.raises(KeyError):
+        credential_store[test_key]
+
+    # Set value
+    test_value = Credential("test_value")
+    credential_store[test_key] = test_value
+
+    # Try to get values from filled store
+    assert test_key in credential_store
+    assert credential_store.get(test_key) == test_value
+    assert credential_store[test_key] == test_value
+
+
+def test_jwt_credential() -> None:
+    """Test get/set with jwt credential."""
+    credential_store = CredentialStore()
+    test_key = "test"
+
+    # Try to get value from empty store
+    assert test_key not in credential_store
+    assert not credential_store.get(test_key)
+    with pytest.raises(KeyError):
+        credential_store[test_key]
+
+    # Set value
+    test_value = JWTCredential(get_jwt())
+    credential_store[test_key] = test_value
+
+    # Try to get values from filled store
+    assert test_key in credential_store
+    assert credential_store.get(test_key) == test_value
+    assert credential_store[test_key] == test_value
+
+    # Try again with expired
+    expired_time = time.time() + 3600
+    with mock.patch("time.time", mock.MagicMock(return_value=expired_time)):
+        assert test_key not in credential_store
+        assert not credential_store.get(test_key)
+        with pytest.raises(KeyError):
+            credential_store[test_key]
+
+
+def test_clear() -> None:
+    """Test clearance of credential store."""
+    credential_store = CredentialStore()
+    test_key = "test"
+    test_permanent_key = "locale"
+
+    # Try to get value from empty store
+    assert test_key not in credential_store
+    assert test_permanent_key not in credential_store
+
+    # Set value
+    test_value = Credential("test_value")
+    test_pemanent_value = Credential("test_locale")
+    credential_store[test_key] = test_value
+    credential_store[test_permanent_key] = test_pemanent_value
+
+    # Try to get values from filled store
+    assert test_key in credential_store
+    assert credential_store[test_key] == test_value
+    assert test_permanent_key in credential_store
+    assert credential_store[test_permanent_key] == test_pemanent_value
+
+    # Try to get values from filled store
+    credential_store.clear()
+
+    # Try to get values from filled store
+    assert test_key not in credential_store
+    assert test_permanent_key in credential_store
+    assert credential_store[test_permanent_key] == test_pemanent_value
+
+
+def test_file_store() -> None:
+    """Test clearance of credential store."""
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # Prepare initial store
+        old_filename = f"{tmpdirname}/.credentials/renault-api.json"
+        old_credential_store = FileCredentialStore(old_filename)
+        test_key = "key"
+        test_value = Credential("value")
+        test_jwt_key = "gigya_jwt"
+        test_jwt_value = JWTCredential(get_jwt())
+        old_credential_store[test_key] = test_value
+        old_credential_store[test_jwt_key] = test_jwt_value
+
+        assert test_key in old_credential_store
+        assert old_credential_store.get(test_key) == test_value
+        assert old_credential_store[test_key] == test_value
+        assert test_jwt_key in old_credential_store
+        assert old_credential_store.get(test_jwt_key) == test_jwt_value
+        assert old_credential_store[test_jwt_key] == test_jwt_value
+
+        # Copy the data into new file
+        new_filename = f"{tmpdirname}/.credentials/renault-api-copy.json"
+        copyfile(old_filename, new_filename)
+        new_credential_store = FileCredentialStore(new_filename)
+
+        # Check that the data is in the new store
+        assert test_key in new_credential_store
+        assert new_credential_store.get(test_key) == test_value
+        assert new_credential_store[test_key] == test_value
+        assert test_jwt_key in new_credential_store
+        assert new_credential_store.get(test_jwt_key) == test_jwt_value
+        assert new_credential_store[test_jwt_key] == test_jwt_value
+
+        # Try again with expired
+        expired_time = time.time() + 3600
+        with mock.patch("time.time", mock.MagicMock(return_value=expired_time)):
+            assert test_key in new_credential_store
+            assert test_jwt_key not in new_credential_store
