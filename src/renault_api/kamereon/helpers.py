8bb03c66d5486f80859887b75a3c2e01a8577274
epenet <6771947+epenet@users.noreply.github.com>
2021-01-21 10:47:17 +0100

    Adjust end-time calculations

--- src/renault_api/kamereon/helpers.py
+++ src/renault_api/kamereon/helpers.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 from typing import Any
 from typing import Dict
+from typing import Optional
 
 from . import models
 
@@ -39,3 +40,23 @@ def create_schedule(
 ) -> models.ChargeSchedule:  # pragma: no cover
     """Update schedule."""
     raise NotImplementedError
+
+
+def get_end_time(start_time: str, duration: Optional[int] = None) -> str:
+    """Compute end time."""
+    total_minutes = get_total_minutes(start_time, duration)
+    return format_time(total_minutes)
+
+
+def format_time(total_minutes: int) -> str:
+    """Format time."""
+    end_hours, end_minutes = divmod(total_minutes, 60)
+    end_hours = end_hours % 24
+    return f"T{end_hours:02g}:{end_minutes:02g}Z"
+
+
+def get_total_minutes(start_time: Optional[str], duration: Optional[int] = None) -> int:
+    """Get total minutes from a `Thh:mmZ` formatted time."""
+    if not start_time:  # pragma: no cover
+        return 0
+    return int(start_time[1:3]) * 60 + int(start_time[4:6]) + (duration or 0)
