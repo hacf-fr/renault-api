84c0af2c4822f08a32772974e88c1331cce951a8
epenet <6771947+epenet@users.noreply.github.com>
2021-01-21 10:47:17 +0100

    Add validation method for list of ChargeSchedule

--- src/renault_api/kamereon/helpers.py
+++ src/renault_api/kamereon/helpers.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 
 from typing import Any
 from typing import Dict
+from typing import List
 from typing import Optional
+from typing import Union
 
 from . import models
 from renault_api.kamereon.exceptions import ModelValidationException
@@ -17,6 +19,8 @@ DAYS_OF_WEEK = [
     "saturday",
     "sunday",
 ]
+
+MINUTES_PER_DAY = 60 * 24
 
 
 def update_schedule(schedule: models.ChargeSchedule, settings: Dict[str, Any]) -> None:
@@ -65,33 +69,81 @@ def get_total_minutes(start_time: Optional[str], duration: Optional[int] = None)
     return int(start_time[1:3]) * 60 + int(start_time[4:6]) + (duration or 0)
 
 
+def validate_charge_schedules(schedules: List[models.ChargeSchedule]) -> None:
+    """Validate a list of ChargeSchedule."""
+    for schedule in schedules:
+        validate_charge_schedule(schedule)
+
+    # Is this really forbidden?
+    # or can multiple schedules be activated as long as there is no overlap?
+    if sum(1 for schedule in schedules if schedule.activated) > 1:
+        raise ModelValidationException("Found multiple activated schedules.")
+
+
 def validate_charge_schedule(schedule: models.ChargeSchedule) -> None:
     """Validate a ChargeSchedule."""
     for day in DAYS_OF_WEEK:
         day_schedule: models.ChargeDaySchedule = getattr(schedule, day)
         if day_schedule:  # pragma: no branch
-            validate_charge_day_schedule(day_schedule)
+            try:
+                validate_charge_day_schedule(day_schedule)
+                end_time = get_total_minutes(
+                    day_schedule.startTime, day_schedule.duration
+                )
+                if end_time > MINUTES_PER_DAY:
+                    # Spans midnight, so we need to compare to next day schedule
+                    next_day_schedule: models.ChargeDaySchedule = getattr(
+                        schedule, get_next_day(day)
+                    )
+                    if next_day_schedule:  # pragma: no branch
+                        prevent_overlap(end_time, next_day_schedule)
+            except ModelValidationException as ex:
+                raise ModelValidationException(
+                    f"Invalid charge settings for {day}: {ex}"
+                ) from ex
 
 
 def validate_charge_day_schedule(schedule: models.ChargeDaySchedule) -> None:
     """Validate a ChargeDaySchedule."""
-    if not (
-        isinstance(schedule.startTime, str)
-        and len(schedule.startTime) == 7
-        and schedule.startTime[0] == "T"
-        and schedule.startTime[3] == ":"
-        and schedule.startTime[6] == "Z"
-        and 0 <= int(schedule.startTime[1:3]) < 24
-        and schedule.startTime[4:6] in ["00", "15", "30", "45"]
+    validate_start_time(schedule.startTime)
+    validate_duration(schedule.duration)
+
+
+def validate_start_time(start_time: Any) -> None:
+    """Validate a charge start time."""
+    if (
+        isinstance(start_time, str)
+        and len(start_time) == 7
+        and start_time[0] == "T"
+        and start_time[3] == ":"
+        and start_time[6] == "Z"
+        and 0 <= int(start_time[1:3]) < 24
+        and start_time[4:6] in ["00", "15", "30", "45"]
     ):
+        return
+    raise ModelValidationException(f"`{start_time}` is not a valid start time.")
+
+
+def validate_duration(duration: Any) -> None:
+    """Validate a charge duration."""
+    if isinstance(duration, int) and duration > 0 and duration % 15 == 0:
+        return
+    raise ModelValidationException(f"`{duration}` is not a valid duration.")
+
+
+def prevent_overlap(
+    end_time: int, next_start_time: Union[int, models.ChargeDaySchedule]
+) -> None:
+    """Prevent overlap."""
+    end_time = end_time % MINUTES_PER_DAY
+    if isinstance(next_start_time, models.ChargeDaySchedule):  # pragma: no branch
+        next_start_time = get_total_minutes(next_start_time.startTime)
+    if end_time >= next_start_time:
         raise ModelValidationException(
-            f"`{schedule.startTime}` is not a valid charge start time"
+            f"`{format_time(end_time)}` end time overlaps with next day schedule."
         )
-    if not (
-        isinstance(schedule.duration, int)
-        and schedule.duration > 0
-        and schedule.duration % 15 == 0
-    ):
-        raise ModelValidationException(
-            f"`{schedule.duration}` is not a valid charge duration"
-        )
+
+
+def get_next_day(day: str) -> str:
+    """Get the next day end time."""
+    return DAYS_OF_WEEK[(DAYS_OF_WEEK.index(day) + 1) % len(DAYS_OF_WEEK)]
---
+++ tests/kamereon/test_kamereon_charge_settings.py
@@ -0,0 +1,76 @@
+"""Tests for Kamereon models."""
+from typing import cast
+
+import pytest
+from tests import fixtures
+
+from renault_api.kamereon import helpers
+from renault_api.kamereon import models
+from renault_api.kamereon import schemas
+from renault_api.kamereon.exceptions import ModelValidationException
+
+
+@pytest.fixture
+def charge_settings() -> models.KamereonVehicleChargingSettingsData:
+    """Test vehicle data for location.json."""
+    response: models.KamereonVehicleDataResponse = fixtures.get_file_content_as_schema(
+        f"{fixtures.KAMEREON_FIXTURE_PATH}/vehicle_data/charging-settings.json",
+        schemas.KamereonVehicleDataResponseSchema,
+    )
+    response.raise_for_error_code()
+
+    return cast(
+        models.KamereonVehicleChargingSettingsData,
+        response.get_attributes(schemas.KamereonVehicleChargingSettingsDataSchema),
+    )
+
+
+def test_validation_success(
+    charge_settings: models.KamereonVehicleChargingSettingsData,
+) -> None:
+    """Test validation of a good schedule list."""
+    helpers.validate_charge_schedules(charge_settings.schedules)
+
+
+def test_validation_invalid_start_time(
+    charge_settings: models.KamereonVehicleChargingSettingsData,
+) -> None:
+    """Test validation of an invalid start time."""
+    charge_settings.schedules[0].monday.startTime = "12:00"
+
+    with pytest.raises(ModelValidationException) as excinfo:
+        helpers.validate_charge_schedules(charge_settings.schedules)
+    assert "is not a valid start time" in str(excinfo.value)
+
+
+def test_validation_invalid_duration(
+    charge_settings: models.KamereonVehicleChargingSettingsData,
+) -> None:
+    """Test validation of an invalid start time."""
+    charge_settings.schedules[0].monday.startTime = "T12:00Z"
+    charge_settings.schedules[0].monday.duration = 14
+    with pytest.raises(ModelValidationException) as excinfo:
+        helpers.validate_charge_schedules(charge_settings.schedules)
+    assert "is not a valid duration" in str(excinfo.value)
+
+
+def test_validation_overlap(
+    charge_settings: models.KamereonVehicleChargingSettingsData,
+) -> None:
+    """Test validation of an overlap with the next day."""
+    charge_settings.schedules[0].wednesday.startTime = "T22:30Z"
+    charge_settings.schedules[0].wednesday.duration = 420
+    charge_settings.schedules[0].thursday.startTime = "T01:00Z"
+    with pytest.raises(ModelValidationException) as excinfo:
+        helpers.validate_charge_schedules(charge_settings.schedules)
+    assert "overlaps with next day schedule" in str(excinfo.value)
+
+
+def test_validation_multiple_active(
+    charge_settings: models.KamereonVehicleChargingSettingsData,
+) -> None:
+    """Test validation of an overlap with the next day."""
+    charge_settings.schedules[1].activated = True
+    with pytest.raises(ModelValidationException) as excinfo:
+        helpers.validate_charge_schedules(charge_settings.schedules)
+    assert "multiple activated schedules" in str(excinfo.value)
