bef68774cfa700d3f011be7ad3fc9f3ed2bc0b97
PysX <celune@gmail.com>
2021-01-27 21:07:02 +0100

    Try fixing tests

--- tests/test_renault_vehicle.py
+++ tests/test_renault_vehicle.py
@@ -68,6 +68,9 @@ async def test_get_contracts(
 ) -> None:
     """Test get_contracts."""
     fixtures.inject_get_vehicle_contracts(mocked_responses, "fr_FR.1.json")
+    fixtures.inject_get_vehicle_details(mocked_responses, "zoe_40.1.json")
+
+    assert await vehicle.get_details()
     assert await vehicle.get_contracts()
 
     # Ensure second call still works (ie. use cached value)
@@ -80,6 +83,7 @@ async def test_has_contract_for_endpoint_1(
 ) -> None:
     """Test has_contract_for_endpoint."""
     fixtures.inject_get_vehicle_contracts(mocked_responses, "fr_FR.2.json")
+    fixtures.inject_get_vehicle_details(mocked_responses, "zoe_40.1.json")
 
     assert await vehicle.has_contract_for_endpoint("battery-status")
     assert await vehicle.has_contract_for_endpoint("hvac-status")
@@ -92,7 +96,7 @@ async def test_has_contract_for_endpoint_2(
 ) -> None:
     """Test has_contract_for_endpoint."""
     fixtures.inject_get_vehicle_contracts(mocked_responses, "fr_FR.1.json")
-
+    fixtures.inject_get_vehicle_details(mocked_responses, "zoe_40.1.json")
     assert await vehicle.has_contract_for_endpoint("battery-status")
     assert not await vehicle.has_contract_for_endpoint("hvac-status")
     assert not await vehicle.has_contract_for_endpoint("charge-mode")
@@ -288,6 +292,7 @@ async def test_set_ac_stop(
 ) -> None:
     """Test set_ac_stop."""
     url = fixtures.inject_set_hvac_start(mocked_responses, "cancel")
+    fixtures.inject_get_vehicle_details(mocked_responses, "zoe_40.1.json")
     assert await vehicle.set_ac_stop()
 
     expected_json = {"data": {"type": "HvacStart", "attributes": {"action": "cancel"}}}
