72e6af970c603aea1250a51593fc71c8a9dfeac6
epenet <epenet@users.noreply.github.com>
2022-01-27 11:17:37 +0100

    Adjust KamereonVehicleHvacStatusData

--- src/renault_api/kamereon/models.py
+++ src/renault_api/kamereon/models.py
@@ -350,10 +350,11 @@ class KamereonVehicleLocationData(KamereonVehicleDataAttributes):
 class KamereonVehicleHvacStatusData(KamereonVehicleDataAttributes):
     """Kamereon vehicle data hvac-status attributes."""
 
-    socThreshold: Optional[float]
+    lastUpdateTime: Optional[str]  # noqa: N815
     externalTemperature: Optional[float]  # noqa: N815
     hvacStatus: Optional[str]  # noqa: N815
     nextHvacStartDate: Optional[str]  # noqa: N815
+    socThreshold: Optional[float]  # noqa: N815
 
 
 @dataclass
