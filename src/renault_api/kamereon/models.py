870d76747742d0d5150abc402dbd7b47ffc2a65f
epenet <epenet@users.noreply.github.com>
2022-01-27 11:28:51 +0100

    Remove Dacia spring from VEHICLE_SPECIFICATIONS

--- src/renault_api/kamereon/models.py
+++ src/renault_api/kamereon/models.py
@@ -59,9 +59,6 @@ VEHICLE_SPECIFICATIONS: Dict[str, Dict[str, Any]] = {
     "XJB1SU": {  # CAPTUR II
         "support-endpoint-hvac-status": False,
     },
-    "XBG1VE": {  # DACIA SPRING
-        "support-endpoint-hvac-status": True,
-    },
 }
 
 
