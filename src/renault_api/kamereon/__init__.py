95db1f9f407b844d96395b07240cfbd2d2205bd3
FRSTR <52470035+FRSTR@users.noreply.github.com>
2022-02-09 14:13:53 +0400

    added endpoint.rst, formatting update

--- src/renault_api/kamereon/__init__.py
+++ src/renault_api/kamereon/__init__.py
@@ -32,8 +32,8 @@ _KCA_GET_ENDPOINTS: Dict[str, Any] = {
     "hvac-settings": {"version": 1},
     "location": {"version": 1},
     "lock-status": {"version": 1},
-    "res-state": {"version": 1},
     "notification-settings": {"version": 1},
+    "res-state": {"version": 1},
 }
 _KCA_POST_ENDPOINTS: Dict[str, Any] = {
     "actions/charge-mode": {"version": 1, "type": "ChargeMode"},
