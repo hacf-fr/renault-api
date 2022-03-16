03f4dbd9f27dc84aa748510892fbb3f55eed0f1d
epenet <6771947+epenet@users.noreply.github.com>
2022-03-16 20:16:19 +0100

    Update default Kamereon API key (#552)
    Co-authored-by: epenet <epenet@users.noreply.github.com>

--- src/renault_api/const.py
+++ src/renault_api/const.py
@@ -18,7 +18,7 @@ PERMANENT_KEYS = [
 
 GIGYA_URL_EU = "https://accounts.eu1.gigya.com"
 GIGYA_URL_US = "https://accounts.us1.gigya.com"
-KAMEREON_APIKEY = "Ae9FDWugRxZQAGm3Sxgk7uJn6Q4CGEA2"
+KAMEREON_APIKEY = "VAX7XYKGfa92yMvXculCkEFyfZbuM7Ss"
 KAMEREON_URL_EU = "https://api-wired-prod-1-euw1.wrd-aws.com"
 KAMEREON_URL_US = "https://api-wired-prod-1-usw2.wrd-aws.com"
 
