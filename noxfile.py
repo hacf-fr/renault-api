154f8e7a0f2ef9b0137ab5112d3469e103218232
epenet <6771947+epenet@users.noreply.github.com>
2023-05-03 17:59:07 +0200

    Adjust

--- noxfile.py
+++ noxfile.py
@@ -174,7 +174,7 @@ def xdoctest(session: Session) -> None:
     session.run("python", "-m", "xdoctest", package, *args)
 
 
-@session(name="docs-build", python="3.10")
+@session(name="docs-build", python="3.11")
 def docs_build(session: Session) -> None:
     """Build the documentation."""
     args = session.posargs or ["docs", "docs/_build"]
