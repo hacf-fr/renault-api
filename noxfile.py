c4440de2fec3c9a3c0a1e8baf9b908789a8044cc
epenet <6771947+epenet@users.noreply.github.com>
2023-08-17 10:51:15 +0200

    Drop Python 3.7 support

--- noxfile.py
+++ noxfile.py
@@ -20,7 +20,7 @@ except ImportError as err:
 
 
 package = "renault_api"
-python_versions = ["3.11", "3.10", "3.9", "3.8", "3.7"]
+python_versions = ["3.11", "3.10", "3.9", "3.8"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
