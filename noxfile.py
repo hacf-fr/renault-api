aa46e470378fe215833a17c447347141a9ac0fa0
Quentame <polletquentin74@me.com>
2023-10-23 23:40:03 +0200

    Fix tests ?

--- noxfile.py
+++ noxfile.py
@@ -119,7 +119,13 @@ def mypy(session: Session) -> None:
     """Type-check using mypy."""
     args = session.posargs or ["src", "tests", "docs/conf.py"]
     session.install(".")
-    session.install("mypy", "pytest", "types-requests", "types-pytz")
+    session.install(
+        "mypy",
+        "pytest",
+        "types-requests",
+        "types-pytz",
+        "pytest-asyncio"
+    )
     session.run("mypy", *args)
     if not session.posargs:
         session.run("mypy", f"--python-executable={sys.executable}", "noxfile.py")
