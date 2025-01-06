cedc1eb16ddb2294f87166d2b3510986eb699168
epenet <6771947+epenet@users.noreply.github.com>
2025-01-06 14:02:30 +0100

    Add Python 3.13 to CI (#1418)
    * Update default Python version to 3.13
    
    * Adjust
    
    * Bump cffi
    
    * Bump nodeenv
    
    * Bump typing-extensions

--- noxfile.py
+++ noxfile.py
@@ -21,7 +21,7 @@ except ImportError as err:
 
 
 package = "renault_api"
-python_versions = ["3.12", "3.11", "3.10", "3.9"]
+python_versions = ["3.13", "3.12", "3.11", "3.10", "3.9"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
@@ -82,7 +82,7 @@ def activate_virtualenv_in_precommit_hooks(session: Session) -> None:
         hook.write_text("\n".join(lines))
 
 
-@session(name="pre-commit", python="3.11")
+@session(name="pre-commit", python="3.13")
 def precommit(session: Session) -> None:
     """Lint using pre-commit."""
     args = session.posargs or ["run", "--all-files", "--show-diff-on-failure"]
@@ -97,7 +97,7 @@ def precommit(session: Session) -> None:
         activate_virtualenv_in_precommit_hooks(session)
 
 
-@session(python="3.12")
+@session(python="3.13")
 def safety(session: Session) -> None:
     """Scan dependencies for insecure packages."""
     requirements = session.poetry.export_requirements()
@@ -180,7 +180,7 @@ def xdoctest(session: Session) -> None:
     session.run("python", "-m", "xdoctest", package, *args)
 
 
-@session(name="docs-build", python="3.12")
+@session(name="docs-build", python="3.13")
 def docs_build(session: Session) -> None:
     """Build the documentation."""
     args = session.posargs or ["docs", "docs/_build"]
@@ -194,7 +194,7 @@ def docs_build(session: Session) -> None:
     session.run("sphinx-build", *args)
 
 
-@session(python="3.12")
+@session(python="3.13")
 def docs(session: Session) -> None:
     """Build and serve the documentation with live reloading on file changes."""
     args = session.posargs or ["--open-browser", "docs", "docs/_build"]
