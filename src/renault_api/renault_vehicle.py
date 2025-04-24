b7f0f4038c257c404d28146d392800209dcd7b2c
epenet <6771947+epenet@users.noreply.github.com>
2025-04-24 15:29:51 +0200

    Replace charging-settings with charge-settings

--- src/renault_api/renault_vehicle.py
+++ src/renault_api/renault_vehicle.py
@@ -5,6 +5,7 @@ from datetime import timezone
 from typing import Any
 from typing import Optional
 from typing import cast
+from warnings import warn
 
 import aiohttp
 
@@ -89,9 +90,7 @@ class RenaultVehicle:
 
         return await self.session.http_request("GET", full_endpoint)
 
-    async def _get_vehicle_data(
-        self, endpoint: str
-    ) -> models.KamereonVehicleDataResponse:
+    async def _get_vehicle_data(self, endpoint: str) -> models.KamereonResponse:
         """GET to /v{endpoint_version}/cars/{vin}/{endpoint}."""
         response = await self._get_vehicle_response(endpoint)
         return cast(
@@ -218,17 +217,23 @@ class RenaultVehicle:
 
     async def get_charging_settings(self) -> models.KamereonVehicleChargingSettingsData:
         """Get vehicle charging settings."""
+        warn(
+            "Method `get_charging_settings` is deprecated, "
+            "please use `get_charge_settings`.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
         response = await self._get_vehicle_data("charging-settings")
         return cast(
             models.KamereonVehicleChargingSettingsData,
             response.get_attributes(schemas.KamereonVehicleChargingSettingsDataSchema),
         )
 
-    async def get_charge_schedule(self) -> dict[str, Any]:
-        """Get vehicle charging schedule."""
-        response = await self._get_vehicle_response("charge-schedule")
+    async def get_charge_settings(self) -> models.KamereonVehicleChargingSettingsData:
+        """Get vehicle charging settings."""
+        response = await self._get_vehicle_response("charging-settings")
         if "data" in response.raw_data and "attributes" in response.raw_data["data"]:
-            return response.raw_data["data"]["attributes"]  # type:ignore[no-any-return]
+            return response.raw_data["data"]["attributes"]
         return response.raw_data
 
     async def get_notification_settings(
