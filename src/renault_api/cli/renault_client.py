59916f394ee5364f58dc5685b9104b0dc6b6cc1b
epenet <6771947+epenet@users.noreply.github.com>
2025-06-03 11:49:36 +0200

    Fix typing issues

--- src/renault_api/cli/renault_client.py
+++ src/renault_api/cli/renault_client.py
@@ -28,7 +28,7 @@ async def get_locale(
 ) -> str:
     """Prompt the user for locale."""
     credential_store: CredentialStore = ctx_data["credential_store"]
-    locale: str | None = credential_store.get_value(CONF_LOCALE)
+    locale: Optional[str] = credential_store.get_value(CONF_LOCALE)
     if locale:
         return locale
 
