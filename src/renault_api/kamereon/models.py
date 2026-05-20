3660c67bf2a0da7548feb7ca47d92338cfce8698
Sagitt <169086+sagitt@users.noreply.github.com>
2026-05-20 06:44:09 +0200

    XCB1VE and XJB1SU endpoints (#2087)
    * XCB1VE and XJB1SU endpoints
    
    * fix descriptions and endopoints
    
    * Update snapshots
    
    ---------
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>

--- src/renault_api/kamereon/models.py
+++ src/renault_api/kamereon/models.py
@@ -384,7 +384,7 @@ _VEHICLE_ENDPOINTS: dict[str, dict[str, EndpointDefinition | None]] = {
         "notification-settings": None,  # Reason: "err.func.vcps.users-helper.get-notification-settings.error"  # noqa: E501
         "pressure": None,  # Reason: "err.func.wired.notFound"
         "res-state": None,  # Reason: "err.func.wired.notFound"
-        "soc-levels": None,  # err.func.wired.forbidden
+        "soc-levels": None,  # Reason: "err.func.wired.forbidden"
     },
     "XFB2BI": {  # Megane IV
         "battery-status": _DEFAULT_ENDPOINTS["battery-status"],
@@ -531,6 +531,11 @@ _VEHICLE_ENDPOINTS: dict[str, dict[str, EndpointDefinition | None]] = {
         "pressure": None,  # err.func.wired.notFound
     },
     "XJB1SU": {  # CAPTUR II
+        "actions/charge-start": None,  # Reason: "err.func.wired.not-found"
+        "actions/charge-stop": None,  # Reason: "err.func.wired.not-found"
+        "actions/horn-start": None,  # Reason: "err.func.wired.not-found"
+        "actions/hvac-start": _DEFAULT_ENDPOINTS["actions/hvac-start"],
+        "actions/lights-start": None,  # Reason: "err.func.wired.not-found"
         "battery-status": _DEFAULT_ENDPOINTS["battery-status"],
         "charge-history": None,  # Reason: "err.func.wired.not-found"
         "charge-mode": None,  # Reason: "err.func.vcps.ev.charge-mode.error"
@@ -546,6 +551,7 @@ _VEHICLE_ENDPOINTS: dict[str, dict[str, EndpointDefinition | None]] = {
         "notification-settings": None,  # Reason: "err.func.vcps.users-helper.get-notification-settings.error"  # noqa: E501
         "pressure": None,  # Reason: "err.func.wired.notFound"
         "res-state": None,  # Reason: "err.func.wired.notFound"
+        "soc-levels": None,  # Reason: "err.func.wired.forbidden"
     },
     "XJL2TR": {  # Arkana E-tech full hybrid
         "cockpit": _DEFAULT_ENDPOINTS["cockpit"],  # confirmed
