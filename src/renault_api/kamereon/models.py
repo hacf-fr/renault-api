8c17e598a8f3a47f9a3f437318fa90065687c601
Paolo Paterna <TopoDiFogna@users.noreply.github.com>
2026-07-31 14:24:56 +0200

    Update action endpoints for CAPTUR II GPL
    Missed actions in previous commit

--- src/renault_api/kamereon/models.py
+++ src/renault_api/kamereon/models.py
@@ -587,9 +587,9 @@ _VEHICLE_ENDPOINTS: dict[str, dict[str, EndpointDefinition | None]] = {
     "XJB2SU": { # CAPTUR II GPL
         "actions/charge-start": None,  # Reason: "err.func.wired.not-found"
         "actions/charge-stop": None,  # Reason: "err.func.wired.not-found"
-        "actions/horn-start": None,  # Reason: "err.func.wired.not-found"
-        "actions/hvac-start": _DEFAULT_ENDPOINTS["actions/hvac-start"],
-        "actions/lights-start": None,  # Reason: "err.func.wired.not-found"
+        "actions/horn-start": _DEFAULT_ENDPOINTS["actions/horn-start"],
+        "actions/hvac-start":  None,  # Reason: "err.func.wired.not-found"
+        "actions/lights-start": _DEFAULT_ENDPOINTS["actions/lights-start"],
         "alerts": None,  # err.func.wired.not-found
         "battery-status": None,  # Reason: "err.func.wired.not-found"
         "charge-history": None,  # Reason: "err.func.wired.not-found"
